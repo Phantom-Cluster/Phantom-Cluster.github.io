c9fa666b3ae32cf9d1343edfdf41aa51fdda5af7
Harsh Trivedi <harsh98trivedi@gmail.com>
2018-10-27 21:27:53 +0530

    Converted the basic site into Jekyll site.

---
+++ _posts/2018-10-27-hello-im-hitanshu.md
@@ -0,0 +1,8 @@
+---
+title: Hello, I'm Hitanshu
+image: https://phantom-cluster.github.io/assets/img/banner.jpg
+author: Hitanshu Sahu
+layout: Post
+---
+
+Hitanshu is a WordPress Enthusiast and FOSS Lover. Apart from WordPress, he loves to work on UI/UX, graphics, and little bit of IoT stuff. He is an active member of Kanpur FOSS community and a coffeeholic.
---
+++ _site/assets/js/app.js
@@ -0,0 +1,127 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+  
+{
+  "particles": {
+    "number": {
+      "value": 40,
+      "density": {
+        "enable": true,
+        "value_area": 700
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 4
+      },
+      "image": {
+        "src": "",
+        "width": 100,
+        "height": 120
+      }
+    },
+    "opacity": {
+      "value": 1.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 4,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 12.181158184520175,
+        "size_min": 0.8120772123013452,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4088700755207083,
+      "width": 0.8017060304327615
+    },
+    "move": {
+      "enable": true,
+      "speed": 3.2,
+      "direction": "top",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 219.26084732136317,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 203.01930307533627,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
+
+);
